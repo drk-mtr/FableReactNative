c94660f378c25fc9388418dc4de0c45b63c6df74
tl <tl@example.com>
2021-03-06 18:45:50 +0000

    Initial

---
+++ FableCode/.config/run.sh
@@ -0,0 +1,1 @@
+dotnet fable watch --outDir ../ExpoCode/GeneratedCode/
---
+++ FableCode/App.fs
@@ -0,0 +1,21 @@
+﻿namespace FableCode
+open Feliz
+open Fable
+open Elmish
+open Fable.ReactNative
+open Fable.ReactNative.Props
+
+module App = 
+
+    [<ReactComponent>]
+    let AppFable() = 
+
+        let (count, setCount) = React.useState(0)
+
+        view [] [
+            text [] ("Count is: " + string count)
+
+            button [ ButtonProperties.OnPress (fun _ -> setCount(count + 1)); ButtonProperties.Title "Increment" ] []
+            button [ ButtonProperties.OnPress (fun _ -> setCount(count - 1)); ButtonProperties.Title "Decrement" ] []
+        ]
+        
